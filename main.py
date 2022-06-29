a72cc78c2deb323c74bb77515553c60c07707ea5
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-06-29 10:07:54 +0800

    新增每周一推播0筆待改

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
+import time, datetime
 import load_excel_gp
 import line_notify_gp
 
@@ -7,9 +7,22 @@ def main():
     n_count =   xls.get_nup_count() # 未修改數量
     last_date = xls.get_last_date() # 最後登錄日期
     sys_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
-    
+
     if n_count == 0:
-        print('nice! it\'s no data.') # 無資料須要維護
+        # 20220629增加功能: 若無資料需要維護，在每個星期一時，會推播訊息，讓使用者知道系統仍然有在工作
+        today = datetime.date.today()
+        # print(today)
+        # print(type(today))
+        # print(today.weekday())
+    
+        if today.weekday() == 0: # 星期一
+            message = f'感謝您，系統檢查0筆回饋待改，Excel最後登記日期為{last_date}，系統檢查時間為{sys_time}'
+            # print(message)
+            line = line_notify_gp.Line()
+            line.post_data(message)
+        else:
+            print('nice! it\'s no data.') # 無資料須要維護
+
         return
     else:
         message = f'圖面回饋尚有 {n_count} 筆未修改！Excel最後登記日期為{last_date}，系統檢查時間為{sys_time}'
