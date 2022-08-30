3affba6a9cb7269127fc9b19a4acfdd7501c1442
rogers Lu <61651257+rogers228@users.noreply.github.com>
2022-08-30 10:08:53 +0800

    修正註解

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
     check_release() #檢查結案待發行
     check_timeout() #檢查待發行已達提醒時間
     bom_gtk()       # 檢查 bom建立作業未確認
-    bom_gfw()       # 檢查回饋待改圖
+    bom_gfw()       # 檢查 bom變更單未確認
     
 if __name__ == '__main__':
     main()
